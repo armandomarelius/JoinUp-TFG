3f9318567df24fd5fe668e268fbdd7cfd6b935fc
armandomarelius <armandomarelius777@gmail.com>
2025-06-04 15:24:23 +0200

    arreglado

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export function useFetch(fetchFunction, params = null) {
   const [data, setData] = useState(null);
